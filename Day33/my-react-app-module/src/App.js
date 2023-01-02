26fe2ba2f52aa1b584cbebb211465277aa39799c
Morning <morning@mstars-lab4-1s-iMac.local>
2023-01-02 12:31:25 +0800

    updated react mini project

--- Day33/my-react-app-module/src/App.js
+++ Day33/my-react-app-module/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Popular Products
+        </h3>
+        <hr></hr>
+        <div class="container">
+          <div class='image'>
+            <img src = "https://ph-files.imgix.net/68d78c27-8cde-4f80-b8af-4728efe30a15.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=120&h=120&fit=crop&dpr=2"></img>
+          </div>
+          <div class="text">
+            <p class=""> 55</p>
+            <p>Haught or Naught</p>
+            <p>Hight-minded or absent-minded? You decide</p>
+            <div class="submit">
+            <p>Submitted by: </p>
+            <img></img>
+            </div>
+          </div>
+        </div>
+
       </header>
     </div>
   );
