657993f89587a152678a816d8e873ada5bac59d0
Morning <morning@mstars-lab4-1s-iMac.local>
2023-01-04 12:14:27 +0800

    added day35

--- Day33/my-react-app-module/src/ProductFunc.jsx
+++ Day33/my-react-app-module/src/ProductFunc.jsx
@@ -1,25 +1,30 @@
-function ProductFunc (){
+
+
+function ProductFunc(props) {
     return (
         <div className='item'>
-                <div className="image thumbnail rounded">
-                    <img src='pics/image-steel.png' className="thumbnail rounded" />
-                </div>
-                <div className="description-container">
-                    <div className="description">
-                        <p>55</p>
-                        <p> Haught or Naught</p>
-                        <p>Hight-minded or absent-minded? You decide</p>
-                        <div className="Submit">
-                            <p>Submitted by:</p>
-                            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" className="profile" />
-
-                        </div>
-
+            <div className="image thumbnail rounded">
+                <img src={props.productImageUrl} className="thumbnail rounded" />
+            </div>
+            <div className="description-container">
+                <div className="description">
+                    <div className="rank">
+                        <i class="bi bi-caret-up-fill"></i>
+                        <p>{props.votes}</p>
+                    </div>
+                    <p>{props.title}</p>
+                    <p>{props.description}</p>
+                    <div className="Submit">
+                        <p>Submitted by:</p>
+                        <img src={props.submitterAvatarUrl} alt="" className="profile" />
 
                     </div>
 
+
                 </div>
+
             </div>
+        </div>
     )
 
 }
