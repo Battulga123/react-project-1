c2a465a46b5fbb18843dba757200c9b2c19eed42
Morning <morning@mstars-lab4-1s-iMac.local>
2023-01-11 09:48:12 +0800

    added day40 and react hook

---
+++ day40/src/data/data.js
@@ -0,0 +1,16 @@
+const userList = [
+    {
+        name: 'Khangaikhuu'
+    },
+    {
+        name: 'Usuhbayar'
+    },
+    {
+        name: 'Turbat'
+    },
+    {
+        name: 'Battulga'
+    }
+]
+
+export default userList
