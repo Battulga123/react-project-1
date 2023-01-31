fcfe7657b14a8d9663ea8482c4a5a79a0af2fb98
Morning <morning@mstars-lab4-1s-iMac.local>
2023-01-31 12:40:12 +0800

    updated fe

---
+++ admin-panel-project/fe/src/pages/Home.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CategoryIcon from "@mui/icons-material/Category";
+
+const drawerWidth = 240;
+
+export default function Home() {
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Admin Panel
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <Typography paragraph>Users</Typography>
+              <Typography paragraph>Products</Typography>
+
+              {/* {["User", "Product"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? (
+                        <AccountCircleIcon />
+                      ) : (
+                        <CategoryIcon />
+                      )}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))} */}
+            </List>
+            <Divider />
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Typography paragraph>text1</Typography>
+          <Typography paragraph>text2</Typography>
+        </Box>
+      </Box>
+    </div>
+  );
+}
---
+++ admin-panel-project/fe/src/pages/Users.jsx
@@ -0,0 +1,10 @@
+import DataTable from "../components/UsersTable";
+
+export default function Users() {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <DataTable/>
+    </div>
+  );
+}
