cc363740d5f076aa2043cef010ae30a8c5bcb6cc
Morning <morning@mstars-lab4-1s-iMac.local>
2023-02-10 11:30:18 +0800

    added day60

--- day-58-useref/src/App.js
+++ day-58-useref/src/App.js
@@ -4,6 +4,8 @@ import { useContext, useState } from "react";
 import List from "./components/List";
 import { ImageContext, ImageContextProvider } from "./contexts/ImageContext";
 import StopWatch from "./components/StopWatch"
+import InputFocus from "./components/InputFocus";
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   // const [isLarge, setIsLarge, imageSize] = useContext(ImageContext);
@@ -19,6 +21,15 @@ function App() {
       <hr></hr>
       <List /> */}
       <StopWatch/>
+
+      <br></br>
+      <InputFocus/>
+
+      <br></br>
+
+      <VideoPlayer/>
+
+     
     </div>
   );
 }
---
+++ day60-mui/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import GridMUI from './component/GridMUI';
+import { SwiperUI } from './component/SwiperUI';
+
+function App() {
+  return (
+    <div className="App">
+     <SwiperUI/>
+    </div>
+  );
+}
+
+export default App;
