d28ef976b25b9f6bdb1da30b5b4c23dceaa5484b
Morning <morning@mstars-lab4-1s-iMac.local>
2023-02-17 10:50:29 +0800

    updated with day65 class component

---
+++ day-65-functional-vs-class/src/App.js
@@ -0,0 +1,12 @@
+import TimersDashboard from "./components/TimersDashboard";
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Day65 - Class Component - Timer App</h1>
+      <TimersDashboard/>
+    </div>
+  );
+}
