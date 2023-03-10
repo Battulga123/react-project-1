21d2536d74a7c52c22360d19130c1432474940e1
Morning <morning@mstars-lab4-1s-iMac.local>
2023-03-10 08:51:42 +0800

    update

--- e-commerce/src/data/carousel.js
+++ e-commerce/src/data/carousel.js
@@ -1,24 +1,24 @@
 const carouselData = [
-    {
-        name: 'Notebook',
-        url: 'image/camera.png',
-        style: 'sliderimg'
-    },
-    {
-        name: 'Speaker',
-        url: 'image/camera.png',
-        style: 'sliderimg'
-    },
-    {
-        name: 'Mobile',
-        url: 'image/camera.png',
-        style: 'sliderimg'
-    },
-    {
-        name: 'Headset',
-        url: 'image/camera.png',
-        style: 'sliderimg'
-    },
-]
+  {
+    name: "Notebook",
+    url: "image/camera.png",
+    style: "sliderimg",
+  },
+  {
+    name: "Speaker",
+    url: "image/camera.png",
+    style: "sliderimg",
+  },
+  {
+    name: "Mobile",
+    url: "image/camera.png",
+    style: "sliderimg",
+  },
+  {
+    name: "Headset",
+    url: "image/camera.png",
+    style: "sliderimg",
+  },
+];
 
-export default carouselData
+export default carouselData;
--- e-commerce/src/data/popular.js
+++ e-commerce/src/data/popular.js
@@ -1,22 +1,20 @@
 const popProducts = [
+  {
+    id: 1,
+    title: "Cameras",
+  },
+  {
+    id: 2,
+    title: "Laptops",
+  },
+  {
+    id: 3,
+    title: "Tablets",
+  },
+  {
+    id: 4,
+    title: "Mouse",
+  },
+];
 
-    {
-        id: 1,
-        title: 'Cameras'
-    },
-    {
-        id: 2,
-        title: 'Laptops'
-    },
-    {
-        id: 3,
-        title: 'Tablets'
-    },
-    {
-        id: 4,
-        title: 'Mouse'
-    }
-]
-
-export default popProducts
-
+export default popProducts;
